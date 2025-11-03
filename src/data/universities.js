39baef9d0bbeebaf98320c0f6545b2be64e8a8bd
kakomonn <167855671+kakomonn@users.noreply.github.com>
2025-11-03 16:19:42 +0800

    Refactor university data to separate module
    Moved the universities data from index.js to a new src/data/universities.js file and updated imports accordingly. This improves maintainability and separation of concerns by keeping data and logic separate.

---
+++ src/data/universities.js
@@ -0,0 +1,348 @@
+export const universities = [
+    {
+      id: 'tokyo',
+      name: '東京大学',
+      color: '#0f4c81',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学系研究科',
+          websiteUrl: 'https://www.t.u-tokyo.ac.jp/soe/admission/general',
+        },
+        {
+          id: 'science',
+          name: '理学系研究科',
+          websiteUrl: 'https://www.s.u-tokyo.ac.jp/ja/admission/master/index.html',
+        },
+        {
+          id: 'ist',
+          name: '情報理工学系研究科',
+          websiteUrl: 'https://www.i.u-tokyo.ac.jp/edu/entra/',
+        },
+        {
+          id: 'frontier',
+          name: '新領域創成科学研究科',
+          websiteUrl: 'https://www.k.u-tokyo.ac.jp/exam/info/',
+        },
+        {
+          id: 'culture',
+          name: '総合文化研究科',
+          websiteUrl: 'https://www.c.u-tokyo.ac.jp/graduate/admission/master-doctor/index.html',
+        },
+        {
+          id: 'iii',
+          name: '学際情報学府',
+          websiteUrl: 'https://www.iii.u-tokyo.ac.jp/admissions',
+        },
+        {
+          id: 'ms',
+          name: '数理科学研究科',
+          websiteUrl: 'https://www.ms.u-tokyo.ac.jp/kyoumu/examination.html',
+        }
+      ]
+    },
+    {
+      id: 'kyoto',
+      name: '京都大学',
+      color: '#8b2323',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学研究科',
+          websiteUrl: 'https://www.t.kyoto-u.ac.jp/ja/admissions/graduate',
+        },
+        {
+          id: 'science',
+          name: '理学研究科',
+          websiteUrl: 'https://sci.kyoto-u.ac.jp/ja/admissions/ms',
+        },
+        {
+          id: 'economics',
+          name: '経済学研究科',
+          websiteUrl: 'https://www.econ.kyoto-u.ac.jp/examguide/graduateexam-info/',
+        },
+        {
+          id: 'informatics',
+          name: '情報学研究科',
+          websiteUrl: 'https://www.i.kyoto-u.ac.jp/admission/',
+        },
+        {
+          id: 'gsm',
+          name: '経営管理大学院',
+          websiteUrl: 'https://www.gsm.kyoto-u.ac.jp/admission/',
+        }
+      ]
+    },
+    {
+      id: 'tohoku',
+      name: '東北大学',
+      color: '#006a4e',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学研究科',
+          websiteUrl: 'https://www.eng.tohoku.ac.jp/admission/grad/master.html',
+        },
+        {
+          id: 'science',
+          name: '理学研究科',
+          websiteUrl: 'https://www.sci.tohoku.ac.jp/juken/graduate-admission.html',
+        },
+        {
+          id: 'bme',
+          name: '医工学研究科',
+          websiteUrl: 'https://www.bme.tohoku.ac.jp/admission/',
+        },
+        {
+          id: 'environmental',
+          name: '環境科学研究科',
+          websiteUrl: 'https://www.kankyo.tohoku.ac.jp/newstudent/nittei-yoko.html',
+        },
+        {
+          id: 'is',
+          name: '情報科学研究科',
+          websiteUrl: 'https://www.is.tohoku.ac.jp/jp/entrance/',
+        }
+      ]
+    },
+    {
+      id: 'osaka',
+      name: '大阪大学',
+      color: '#1e3f66',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学研究科',
+          websiteUrl: 'https://www.eng.osaka-u.ac.jp/ja/entrance/g_admissions/',
+        },
+        {
+          id: 'science',
+          name: '理学研究科',
+          websiteUrl: 'https://www.sci.osaka-u.ac.jp/ja/admissions/admissions_d/',
+        },
+        {
+          id: 'medicine',
+          name: '医学系研究科',
+          websiteUrl: 'https://www.med.osaka-u.ac.jp/admission/admission',
+        },
+        {
+          id: 'es',
+          name: '基礎工学研究科',
+          websiteUrl: 'https://www.es.osaka-u.ac.jp/ja/examinee/graduate-school-of-engineering-science/entrance-exam/',
+        },
+        {
+          id: 'ist',
+          name: '情報科学研究科',
+          websiteUrl: 'https://www.ist.osaka-u.ac.jp/japanese/examinees/admission/',
+        }
+      ]
+    },
+    {
+      id: 'nagoya',
+      name: '名古屋大学',
+      color: '#654321',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学研究科',
+          websiteUrl: 'https://www.engg.nagoya-u.ac.jp/prospective/',
+        },
+        {
+          id: 'science',
+          name: '理学研究科',
+          websiteUrl: 'https://www.sci.nagoya-u.ac.jp/graduate/index.html',
+        },
+        {
+          id: 'env',
+          name: '環境学研究科',
+          websiteUrl: 'https://www.env.nagoya-u.ac.jp/admission/index.html',
+        },
+        {
+          id: 'i',
+          name: '情報学研究科',
+          websiteUrl: 'https://www.i.nagoya-u.ac.jp/gs/entranceexamination/',
+        },
+        {
+          id: 'math',
+          name: '多元数理科学研究科',
+          websiteUrl: 'https://www.math.nagoya-u.ac.jp/ja/admission/',
+        }
+      ]
+    },
+    {
+      id: 'isct',
+      name: '東京科学大学 旧・東京工業大学',  
+      color: '#7b3f00',
+      departments: [
+        {
+          id: 'isct',
+          name: '理工学系',   
+          websiteUrl: 'https://www.titech.ac.jp/admissions/prospective-students/admissions/guide',  
+        }
+      ]
+    },
+    {
+      id: 'hokkaido',
+      name: '北海道大学',
+      color: '#4169e1',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学院',
+          websiteUrl: 'https://www.eng.hokudai.ac.jp/graduate/examinfo/',
+        },
+        {
+          id: 'science',
+          name: '理学院',
+          websiteUrl: 'https://www2.sci.hokudai.ac.jp/gs/admission-guideline',
+        },
+        {
+          id: 'fisheries',
+          name: '水産科学院',
+          websiteUrl: 'https://www2.fish.hokudai.ac.jp/admission/mcdc.html',
+        },
+        {
+          id: 'ist',
+          name: '情報科学院',
+          websiteUrl: 'https://www.ist.hokudai.ac.jp/examinfo/',
+        },
+        {
+          id: 'hops',
+          name: '公共政策大学院',
+          websiteUrl: 'https://www.hops.hokudai.ac.jp/admission/',
+        }
+      ]
+    },
+    {
+      id: 'kyushu',
+      name: '九州大学',
+      color: '#800080',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学府',
+          websiteUrl: 'https://www.eng.kyushu-u.ac.jp/admissions.html',
+        },
+        {
+          id: 'science',
+          name: '理学府',
+          websiteUrl: 'https://www.sci.kyushu-u.ac.jp/admission/daigakuin_master.html',
+        },
+        {
+          id: 'economics',
+          name: '経済学府',
+          websiteUrl: 'https://www.econ.kyushu-u.ac.jp/nyushi/',
+        },
+        {
+          id: 'isee',
+          name: 'システム情報科学府',
+          websiteUrl: 'https://www.isee.kyushu-u.ac.jp/admissions_master.html',
+        },
+        {
+          id: 'math',
+          name: '数理学府',
+          websiteUrl: 'https://www.math.kyushu-u.ac.jp/admission/graduateschool/',
+        }
+      ]
+    },
+    {
+      id: 'waseda',
+      name: '早稲田大学',
+      color: '#c41e3a',
+      departments: [
+        {
+          id: 'fundamental',
+          name: '基幹理工学研究科',
+          websiteUrl: 'https://www.waseda.jp/fsci/admissions_gs/',
+        },
+        {
+          id: 'creative',
+          name: '創造理工学研究科',
+          websiteUrl: 'https://www.waseda.jp/fsci/admissions_gs/',
+        },
+        {
+          id: 'advanced',
+          name: '先進理工学研究科',
+          websiteUrl: 'https://www.waseda.jp/fsci/admissions_gs/',
+        }
+      ]
+    },
+    {
+      id: 'tsukuba',
+      name: '筑波大学',
+      color: '#228b22',
+      departments: [
+        {
+          id: 'engineering',
+          name: '人間総合科学学術院',
+          websiteUrl: 'https://www.ap-graduate.tsukuba.ac.jp/course/',
+        },
+        {
+          id: 'humanities',
+          name: '人文社会ビジネス科学学術院',
+          websiteUrl: 'https://www.ap-graduate.tsukuba.ac.jp/course/',
+        },
+        {
+          id: 'gstils',
+          name: '理工情報生命学術院',
+          websiteUrl: 'https://www.ap-graduate.tsukuba.ac.jp/course/',
+        }
+      ]
+    },
+    {
+      id: 'uec',
+      name: '電気通信大学',
+      color: '#483d8b',
+      departments: [
+        {
+          id: 'informatics',
+          name: '情報理工学研究科',
+          websiteUrl: 'https://www.uec.ac.jp/education/graduate/admission/request.html',
+        }
+      ]
+    },
+    {
+      id: 'kobe',
+      name: '神戸大学',
+      color: '#3cb371',
+      departments: [
+        {
+          id: 'engineering',
+          name: '工学研究科',
+          websiteUrl: 'http://www.eng.kobe-u.ac.jp/examinee.html',
+        },
+        {
+          id: 'science',
+          name: '理学研究科',
+          websiteUrl: 'http://www.sci.kobe-u.ac.jp/admissions/master.html',
+        },
+        {
+          id: 'economics',
+          name: '経済学研究科',
+          websiteUrl: 'https://www.econ.kobe-u.ac.jp/admission-master/',
+        },
+        {
+          id: 'csi',
+          name: 'システム情報学研究科',
+          websiteUrl: 'https://www.csi.kobe-u.ac.jp/exam/master_exam.html',
+        }
+      ]
+    },
+    {
+      id: 'hiroshima',
+      name: '広島大学',
+      color: '#ff7f50',
+      departments: [
+        {
+          id: 'engineering',
+          name: '先進理工系科学研究科',
+          websiteUrl: 'https://www.hiroshima-u.ac.jp/adse/admission',
+        },
+        {
+          id: 'humanities',
+          name: '人間社会科学研究科',
+          websiteUrl: 'https://www.hiroshima-u.ac.jp/gshs/nyuusi',
+        }
+      ]
+    }
+  ];
